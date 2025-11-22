860c7a3f336dbf11f28e0a6b71392776f8c00b11
Cristian Arboleda <Cristian_calderon82192@elpoli.edu.co>
2025-11-21 19:42:24 -0500

    feat: Add Blazor pages for managing branch and movement catalog types.

--- obj/Debug/net9.0/byte_bank_blazor.AssemblyInfo.cs
+++ obj/Debug/net9.0/byte_bank_blazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("byte_bank_blazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ff9203fdccce4693f2dd11f8e49b3b977242807")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45c7f64546126162ed9c3126c9afb33401278540")]
 [assembly: System.Reflection.AssemblyProductAttribute("byte_bank_blazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("byte_bank_blazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
