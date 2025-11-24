09da6cac136abe7164e33ac530e2b0dc3e4cfe0f
Cristian Arboleda <Cristian_calderon82192@elpoli.edu.co>
2025-11-23 21:53:19 -0500

    feat: Add Movimientos Blazor page filters

--- obj/Debug/net9.0/byte_bank_blazor.AssemblyInfo.cs
+++ obj/Debug/net9.0/byte_bank_blazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("byte_bank_blazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c91d38b203995d7a2369361d5cb51e0ebd8aca99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1c73485e4b5d3534637a3b712923fd963dcee45")]
 [assembly: System.Reflection.AssemblyProductAttribute("byte_bank_blazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("byte_bank_blazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
